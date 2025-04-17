3fc8fe98de3ab62c29e3fdc8890d61255ebef6bc
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-17 10:16:40 +0800

    feat: add 3rd inline notes of section 2

--- 02-basics-n-core-concepts/basics-05-using-the-native-event-object/app.js
+++ 02-basics-n-core-concepts/basics-05-using-the-native-event-object/app.js
@@ -2,10 +2,30 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      fullName: '',
     };
   },
+  // computed: {
+  //   fullName() {
+  //     // REMARK: Separate functions with different usages.
+  //     return this.name === '' ?
+  //       '' : (this.name + " " + "Schwarzmuller");
+  //   },
+  // }, // REMARK: Computerd properties are like methods but their dependencies are awared.
+  watch: {
+    name(value) {
+      // REMARK: Whenever name changes, watcher executes.
+      this.fullName = value === '' ?
+        '' : (value + ' ' + "Schwarzmuller");
+    }
+  },
   methods: {
+    outputFullName() {
+      // REMARK: Separate functions with different usages.
+      return this.name === '' ?
+        '' : (this.name + " " + "Schwarzmuller");
+    },
     setName(event, lastName) {
       this.name = event.target.value;
     },
@@ -21,7 +41,7 @@ const app = Vue.createApp({
       // document.querySelector('input').value = '';
       this.name = '';
     }
-  }
+  }, // REMARK: Should still bind events to method tho.
 });
 
 app.mount('#events');
