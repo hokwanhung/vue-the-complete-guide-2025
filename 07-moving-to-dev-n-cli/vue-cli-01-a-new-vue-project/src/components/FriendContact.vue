7ae9307a459620a44ffbc27237e7a2639a5f08e9
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-18 23:06:23 +0800

    feat: complete section 7 code files and inline notes

---
+++ 07-moving-to-dev-n-cli/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ isDetailsVisible ?
+      'Hide' : 'Show' }} Details</button>
+    <ul v-if="isDetailsVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDetailsVisible: false,
+      friend: {
+        id: 'manual',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
+      methods: {
+        toggleDetails() {
+          this.isDetailsVisible = !this.isDetailsVisible;
+        }
+      }
+    };
+  }
+};
+</script>
