b690c13347fd02d73dc1c5e9c138dfe99d40be45
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-18 23:06:23 +0800

    feat: complete section 7 code files and inline notes

--- 07-moving-to-dev-n-cli/vue-cli-01-a-new-vue-project/src/main.js
+++ 07-moving-to-dev-n-cli/vue-cli-01-a-new-vue-project/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+import FriendContact from './components/FriendContact.vue';
 
-createApp({}).mount('#app');
+const app = createApp(App)
+
+// REMARK: Enable the FriendContact element.
+app.component('friend-contact', FriendContact);
+
+app.mount('#app');
 
 
