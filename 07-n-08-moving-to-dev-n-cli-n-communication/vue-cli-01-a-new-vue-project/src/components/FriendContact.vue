0e9e07f37eae91ebe009c72a517ac09b6913b52e
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-18 23:31:46 +0800

    feat: add 1st inline notes of section 8 (with code files updates)

--- 07-n-08-moving-to-dev-n-cli-n-communication/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ 07-n-08-moving-to-dev-n-cli-n-communication/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,32 +1,45 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }} {{ isFavouriteFriend === '1' ?
+      '(Favourite)' : '' }}</h2>
+    <button @click="toggleFavourite">Toggle Favourite</button>
     <button @click="toggleDetails">{{ isDetailsVisible ?
       'Hide' : 'Show' }} Details</button>
     <ul v-if="isDetailsVisible">
-      <li><strong>Phone:</strong> {{ friend.phone }}</li>
-      <li><strong>Email:</strong> {{ friend.email }}</li>
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
     </ul>
   </li>
 </template>
 
 <script>
 export default {
+  // REMARK: props accept custom properties, and can use in array by default as format.
+  props: [
+    // REMARK: Define as camel-case inside the script, but use dash (kebab-case) in element properties.
+    'name',
+    'phoneNumber',
+    'emailAddress',
+    'isFavourite',
+  ],
   data() {
     return {
       isDetailsVisible: false,
-      friend: {
-        id: 'manual',
-        name: 'Manuel Lorenz',
-        phone: '01234 5678 991',
-        email: 'manuel@localhost.com',
-      },
-      methods: {
-        toggleDetails() {
-          this.isDetailsVisible = !this.isDetailsVisible;
-        }
-      }
-    };
+      isFavouriteFriend: this.isFavourite,
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.isDetailsVisible = !this.isDetailsVisible;
+    },
+    toggleFavourite() {
+      // REMARK: Will return error because (unidirectional data flow),
+      // data from app should only be changed in app (immutable props).
+      // this.isFavourite === '1' ?
+      //   this.isFavourite = '0' : this.isFavourite = '1';
+      this.isFavouriteFriend === '1' ?
+        this.isFavouriteFriend = '0' : this.isFavouriteFriend = '1';
+    }
   }
 };
 </script>
