ca4fa4463f4531f2d8467749891e8b8c36c7ab7e
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-19 12:10:39 +0800

    feat: add 3rd inline notes of section 8 (with code files updates)

---
+++ 07-n-08-moving-to-dev-n-cli-n-communication/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div><!-- REMARK: Use <div> instead of <section>. (from solution) -->
+    <active-user :username="username" :age="age"></active-user>
+    <user-data @update-user-data="updateData"></user-data>
+  </div>
+</template>
+
+<script>
+import ActiveUser from './components/ActiveUser.vue';
+import UserData from './components/UserData.vue';
+export default {
+  components: { ActiveUser, UserData },
+  data() {
+    return {
+      username: '',
+      age: 0,
+    };
+  },
+  methods: {
+    updateData(userData) {
+      this.username = userData.username;
+      this.age = userData.age;
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+</style>
