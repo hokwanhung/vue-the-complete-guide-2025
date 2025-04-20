1b9280cfd6078246b7c368de4fc1ae26ea36108e
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-20 16:50:37 +0800

    feat: add 1st inline notes of section 9 (with code files updates)

--- 09-diving-deeper/cmp-adv-01-starting-setup/src/components/TheHeader.vue
+++ 09-diving-deeper/cmp-adv-01-starting-setup/src/components/TheHeader.vue
@@ -4,18 +4,22 @@
   </header>
 </template>
 
-<style>
-  header {
-    width: 100%;
-    height: 5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #14005e;
-  }
+<!-- REMARK: By default <style> would be treated as globally. -->
+<!-- REMARK: Through scoped, the styles will only affect the current template,
+  no parent, child or siblings components are affected. -->
+<!-- REMARK: Vue achieve this by giving special attributes like data-, V-, etc. -->
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #14005e;
+}
 
-  header h1 {
-    color: white;
-    margin: 0;
-  }
+header h1 {
+  color: white;
+  margin: 0;
+}
 </style>
--- 09-diving-deeper/cmp-adv-01-starting-setup/src/main.js
+++ 09-diving-deeper/cmp-adv-01-starting-setup/src/main.js
@@ -1,17 +1,24 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import TheHeader from './components/TheHeader.vue';
+// import TheHeader from './components/TheHeader.vue';
 import BaseBadge from './components/BaseBadge.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+import BaseCard from './components/BaseCard.vue';
 
 const app = createApp(App);
 
 // REMARK: These are the global components that are registered everywhere.
-app.component('the-header', TheHeader);
+// REMARK: But this might be unclear when the project gets bigger.
+// app.component('the-header', TheHeader);
+
+// REMARK: This is used in multiple places as a global component, 
+// so it should be registered here.
 app.component('base-badge', BaseBadge);
-app.component('badge-list', BadgeList);
-app.component('user-info', UserInfo);
+app.component('base-card', BaseCard);
+
+// app.component('badge-list', BadgeList);
+// app.component('user-info', UserInfo);
 
 app.mount('#app');
