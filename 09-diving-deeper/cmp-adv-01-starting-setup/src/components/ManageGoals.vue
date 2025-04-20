06907b1e1956fdbf1f05c33b020185ec3448e810
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-20 17:40:05 +0800

    feat: complete section 9 code files with inline notes

--- 09-diving-deeper/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ 09-diving-deeper/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,15 +1,20 @@
 <template>
+  <!-- REMARK: Allow to have multiple top-level elements through V3 (fragments). -->
   <div>
     <h2>Manage Goals</h2>
     <!-- REMARK: input field is removed from the dom, 
      and the content is destroyed when switched. -->
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <ErrorAlert v-if="isInputInvalid">
-      <h2>Input is invalid!</h2>
-      <p>Please enter at least a few characters...</p>
-      <button @click="confirmError">Okay</button>
-    </ErrorAlert>
+    <!-- REMARK: Because this should be at the level of App.vue,
+     and this can be rendered in elsewhere but still using the scripts in this component. -->
+    <Teleport to="body">
+      <ErrorAlert v-if="isInputInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </ErrorAlert>
+    </Teleport>
   </div>
 </template>
 
