4dae3a67c089f603ed216f95aebe1b69aa3aab76
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-20 16:50:37 +0800

    feat: add 1st inline notes of section 9 (with code files updates)

--- 09-diving-deeper/cmp-adv-01-starting-setup/src/App.vue
+++ 09-diving-deeper/cmp-adv-01-starting-setup/src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
+    <!-- <TheHeader /> -->
     <the-header></the-header>
     <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
   </div>
+  <course-goals #default="slotProps">
+    <!-- REMARK: By default all props are inside slotProps, and it doesn't have to be specified. -->
+    <!-- <template #default="slotProps"> -->
+    <h2>{{ slotProps.item }}</h2>
+    <!-- <p>{{ slotProps['another-prop'] }}</p> -->
+    <p>{{ slotProps.anotherProp }}</p> <!-- in my case it becomes camelCase by default. -->
+    <!-- </template> -->
+  </course-goals>
 </template>
 
 <script>
+import BadgeList from './components/BadgeList.vue';
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
 export default {
+  // REMARK: This way we register the components locally.
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals,
+    // 'the-header': TheHeader, // or TheHeader as key
+  },
   data() {
     return {
       activeUser: {
@@ -24,6 +42,8 @@ export default {
 };
 </script>
 
+<!-- REMARK: Styles that affect the entire app,
+ should define here because App.vue is the entry point. -->
 <style>
 html {
   font-family: sans-serif;
