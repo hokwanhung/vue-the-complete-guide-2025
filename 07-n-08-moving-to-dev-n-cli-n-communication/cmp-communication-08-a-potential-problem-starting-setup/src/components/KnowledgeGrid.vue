d4cd5f28e592eff72c4a76bb26c81b0853704a03
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-20 11:39:20 +0800

    feat: complete section 8 code files with inline notes

---
+++ 07-n-08-moving-to-dev-n-cli-n-communication/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <knowledge-element v-for="topic in topics" :key="topic.id" :id="topic.id" :topic-name="topic.title"
+      :description="topic.description" @select-topic="$emit('select-topic', $event)"></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  // REMARK: Can only inject what's provided on a higher level (parent-child relationships).
+  inject: ['topics'],
+  // props: ['topics'],
+  // emits: ['select-topic']
+};
+</script>
