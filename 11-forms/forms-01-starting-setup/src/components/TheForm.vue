9cddf26b3bf704e1095708d57d8dccb63e721b41
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-21 12:57:04 +0800

    feat: complete section 11 code files with inline notes

--- 11-forms/forms-01-starting-setup/src/components/TheForm.vue
+++ 11-forms/forms-01-starting-setup/src/components/TheForm.vue
@@ -1,16 +1,21 @@
 <template>
-  <form>
-    <div class="form-control">
+  <form @submit.prevent="submitForm">
+    <!-- REMARK: refs are better for reading data once, without modifications and others. -->
+    <div class="form-control" :class="{ invalid: usernameValidity === 'invalid' }">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <!-- REMARK: There's also sub-methods in v-model like lazy, number and trim. -->
+      <input id="user-name" name="user-name" type="text" v-model.trim="username" @blur="validateInput" />
+      <p v-if="usernameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <!-- REMARK: Even it seems like number, the type would be string with ref. -->
+      <!-- REMARK: v-model will auto detects the number as type number and will convert. -->
+      <input id="age" name="age" type="number" v-model="age" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -18,39 +23,89 @@
     </div>
     <div class="form-control">
       <h2>What are you interested in?</h2>
+      <!-- REMARK: Use empty arrays for checkbox. -->
+      <!-- REMARK: Also have to set values to assign values for Vue to add/remove. -->
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest" />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="interest" />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest" />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
+      <!-- REMARK: Single checkbox returns true or false. -->
+      <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm" />
+      <label for="confirm-terms">Agree to terms of use?</label>
+    </div>
+    <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value="video" v-model="how" />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how" />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value="other" v-model="how" />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <!-- REMARK: Vue supports v-model on custom components. -->
+      <rating-control v-model="rating"></rating-control>
     </div>
     <div>
       <button>Save Data</button>
     </div>
   </form>
 </template>
+
+<script>
+import RatingControl from './RatingControl.vue';
+
+export default {
+  components: {
+    RatingControl,
+  },
+  name: 'the-form',
+  data() {
+    return {
+      username: '',
+      // REMARK: To be clear that it has no value yet.
+      userAge: null,
+      referrer: '',
+      interest: [],
+      how: null,
+      confirm: false,
+      usernameValidity: 'pending',
+      rating: null,
+    }
+  },
+  methods: {
+    submitForm() {
+      this.username = '';
+      this.age = null;
+      this.referrer = 'wom'; // reset back to 'word of mouth'.
+      this.interest = [];
+      this.how = null;
+      this.confirm = false;
+      this.rating = null;
+    },
+    validateInput() {
+      this.username === '' ?
+        this.usernameValidity = 'invalid' : this.usernameValidity = 'valid';
+    }
+  }
+}
+</script>
 
 <style scoped>
 form {
@@ -64,6 +119,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: rgb(131, 99, 99);
 }
 
 label {
@@ -94,8 +157,8 @@ input[type='radio'] {
   margin-right: 1rem;
 }
 
-input[type='checkbox'] + label,
-input[type='radio'] + label {
+input[type='checkbox']+label,
+input[type='radio']+label {
   font-weight: normal;
 }
 
