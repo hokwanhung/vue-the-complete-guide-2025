ed9cdaeeb31c86e8448588d80b1e34cd68485c6f
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-28 14:06:21 +0800

    fix: fix broken code files after security cleaning with git-filter-repo

--- 18-optimisation-n-deployment/deploy-01-starting-project/src/store/modules/auth/actions.js
+++ 18-optimisation-n-deployment/deploy-01-starting-project/src/store/modules/auth/actions.js
@@ -16,11 +16,11 @@ export default {
   async auth(context, payload) {
     const mode = payload.mode;
     let url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=\' + process.env.VUE_APP_WEB_API_KEY;
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + process.env.VUE_APP_WEB_API_KEY;
 
     if (mode === 'signup') {
       url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=\' + process.env.VUE_APP_WEB_API_KEY;
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.VUE_APP_WEB_API_KEY;
     }
     const response = await fetch(url, {
       method: 'POST',
