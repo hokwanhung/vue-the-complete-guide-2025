9466fc448e6479ff648b8382e89ce6eb4f423040
hokwanhung <hokwanhung.2019@gmail.com>
2025-04-28 08:26:15 +0800

    feat: add 2nd inline notes of section 19 (with code files updates)

--- 19-composition-api/composition-01-starting-setup/src/App.vue
+++ 19-composition-api/composition-01-starting-setup/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
+    <!-- <h2>{{ user.name }}</h2> -->
+    <h2>{{ userName }}</h2>
     <h2>{{ user.age }}</h2>
     <button @click="setAge">Change age</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />
+    </div>
   </section>
 </template>
 
@@ -10,10 +15,17 @@
 <script>
 // REMARK: reactive is explicitly made for objects.
 // eslint-disable-next-line no-unused-vars
-import { ref, reactive, isReactive, isRef, toRefs } from 'vue';
+import { ref, reactive, isReactive, isRef, toRefs, computed } from 'vue';
 
 export default {
   setup() {
+    const firstName = ref('');
+    const lastName = ref('');
+
+    const uName = computed(() => {
+      return firstName.value + ' ' + lastName.value;
+    }) // REMARK: Computed refs are read-only.
+
     // REMARK: ref() create a reference to values,
     // the below creates a reactive string.
     const user = ref({ // must pass an object if with reactive.
@@ -34,18 +46,29 @@ export default {
     //   // no extra wrappers for objects.
     // }, 2000);
 
+    function setFirstName(event) {
+      firstName.value = event.target.value;
+    }
+
+    function setLastName(event) {
+      lastName.value = event.target.value;
+    }
+
     function setNewAge() {
       // uAge.value = 32;
-      user.age = 32;
+      user.value.age = 32;
     }
 
     // REMARK: Put all things to expose to the template.
     // REMARK: This is drilled into as value is a Proxy,
     // thus the values cannot be updated when using user.value.xxx,
     // the reactivity is only known to user - same with reactive.
-    return { 
+    return {
       // userName: user.value.name, age: user.value.age, 
-      user: user, setAge: setNewAge };
+      userName: uName,
+      user: user, setAge: setNewAge,
+      setFirstName, setLastName,
+    };
   },
   // data() {
   //   return {
